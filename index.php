70956725ccad666dcdfc3f6b80776c4d11fc2a84
why144 <wahyudi140319@gmail.com>
2023-09-27 13:44:49 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste veritatis, esse distinctio quisquam illo, recusandae corporis inventore, quod voluptatem ducimus labore officiis quasi expedita delectus quo velit? Quos, nemo architecto.</p>
 </body>
 </html>
